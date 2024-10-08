35dd0e98ee5db82deeb580c98336ae99f5c0758a
Abhijit <abhijitparida00@gmail.com>
2024-10-08 14:31:19 +0530

    LeetCode practice code

--- com/practice/leetcode/ClimbStairs.java
+++ com/practice/leetcode/ClimbStairs.java
@@ -1,4 +1,57 @@
 package com.practice.leetcode;
 
+/**
+    You are climbing a staircase. It takes n steps to reach the top.
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
+
 public class ClimbStairs {
+
+    // 1st Way
+    public static int climbStairs1(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];  // dp[i] represents the number of ways to reach step i
+        dp[1] = 1;
+        dp[2] = 2;
+
+        // Fill the dp array using the relation dp[i] = dp[i - 1] + dp[i - 2]
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];  // The number of ways to reach the nth step
+    }
+
+    // 2nd Way
+    public static int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int first = 1;
+        int second = 2;
+
+        // Use two variables to store the last two values instead of an array
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(climbStairs1(n));  // Output: 8
+        System.out.println(climbStairs2(n));  // Output: 8
+
+        n = 3;
+        System.out.println(climbStairs1(n));  // Output: 3
+        System.out.println(climbStairs2(n));  // Output: 3
+    }
 }
+
---
+++ com/practice/leetcode/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,60 @@
+package com.practice.leetcode;
+
+class ListNum {
+    int val;
+    ListNum next;
+
+    ListNum(int val) {
+        this.val = val;
+    }
+}
+
+public class RemoveDuplicatesFromSortedList{
+
+    public static ListNum deleteDuplicates(ListNum head) {
+        // If the list is empty or contains only one node, return it as it is
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNum current = head;
+
+        // Traverse the list
+        while (current != null && current.next != null) {
+            // If the current node has the same value as the next node, bypass the next node
+            if (current.val == current.next.val) {
+                current.next = current.next.next;  // Skip the duplicate node
+            } else {
+                current = current.next;  // Move to the next node
+            }
+        }
+
+        return head;
+    }
+
+    public static void printList(ListNum head) {
+        ListNum current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Creating a sorted linked list: 1 -> 1 -> 2
+        ListNum head = new ListNum(1);
+        head.next = new ListNum(1);
+        head.next.next = new ListNum(2);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        // Remove duplicates
+        head = deleteDuplicates(head);
+
+        System.out.println("List after removing duplicates:");
+        printList(head);
+    }
+}
+
